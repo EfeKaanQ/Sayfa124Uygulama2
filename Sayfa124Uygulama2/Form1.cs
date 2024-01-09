39a06bfcca55c6e9e559fe94ef22545d6b2215c4
efe33 <efe33@BPC04>
2024-01-09 09:49:37 +0300

    Proje dosyası ekle.

---
+++ Sayfa124Uygulama2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayfa124Uygulama2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimler = new string[5]; 
+        int index = 0;
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            isimler[index] = txtAdSoyad.Text;
+            index++;
+        }
+
+        private void btnListe_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < isimler.Length; i++)
+            {
+                lbListe.Items.Add(isimler[i]);
+            }
+        }
+    }
+}
